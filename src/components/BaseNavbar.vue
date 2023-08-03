310f5ced0ea6dbd9e0b95ef1fdf2147fdb27256d
Kaio <rasec.k0@proton.me>
2023-08-03 19:10:03 -0300

    first commit

---
+++ src/components/BaseNavbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-slate-900 w-screen h-10 bottom-0 fixed md:h-screen md:w-10 ">
+   
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
